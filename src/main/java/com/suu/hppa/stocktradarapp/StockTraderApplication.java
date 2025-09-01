82d61e0c2b29d89e0684ee0b9f21cfe950f434b2
kobatakeyann <suu_hppa@outlook.jp>
2025-09-01 21:34:11 +0900

    Rename entry file

--- src/main/java/com/suu/hppa/stocktradarapp/StockTraderApplication.java
+++ src/main/java/com/suu/hppa/stocktradarapp/StockTraderApplication.java
@@ -1,21 +1,21 @@
-package com.suu.hppa.summer_training;
+package com.suu.hppa.stocktradarapp;
 
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.suu.hppa.summer_training.presentation.cli.controller.CliAppController;
+import com.suu.hppa.stocktradarapp.presentation.cli.controller.CliAppController;
 
 @SpringBootApplication
-public class SummerTrainingApplication implements ApplicationRunner {
+public class StockTraderApplication implements ApplicationRunner {
 	private final CliAppController appController;
 
-	public SummerTrainingApplication(CliAppController appController) {
+	public StockTraderApplication(CliAppController appController) {
 		this.appController = appController;
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(SummerTrainingApplication.class, args);
+		SpringApplication.run(StockTraderApplication.class, args);
 	}
 
 	@Override
